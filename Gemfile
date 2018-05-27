b13c222b270c35abf91080b1b8574733d83b9e6e
Robin Rundkvist <robin@varvet.se>
2018-05-27 13:38:24 +0200

    Set up middleman

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-gh_pages'
+gem 'middleman-scavenger', :git => "git://github.com/varvet/middleman-scavenger.git", :branch => "feature/middleman-4-support"
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
---
+++ config.rb
@@ -0,0 +1,48 @@
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascript"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+
+activate :autoprefixer do |prefix|
+  config.browsers = ['last 5 versions', 'Explorer >= 10']
+  config.cascade = false
+  config.grid = true
+end
+
+activate :gh_pages do |gh_pages|
+  gh_pages.remote = 'git@github.com:karlrobin/robinrundkvist.se.git'
+end
+
+activate :middleman_scavenger do |config|
+  config.path = "./source/assets/images/icons"
+  config.sprite_path = "assets/images/icon-sprite.svg"
+end
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+configure :development do
+  # activate :livereload
+end
+
+configure :build do
+  sass = {
+    :output_style => :compact,
+    :line_comments => false
+  }
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+end
