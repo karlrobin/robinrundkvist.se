08684671dd60affa2002ea89802341994b6a500a
Robin Rundkvist <robin.rundkvist@gmail.com>
2020-12-14 14:25:32 +0100

    Deploy on 2020-12-14 14:25:32 +0100

---
+++ assets/javascript/main.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector("#scroll-to-contact").addEventListener("click",function(e){e.preventDefault(),document.querySelector("#contact").scrollIntoView({behavior:"smooth"})})});
